8ee62be93cad0c4a2ebc9003d045dd83e79e6299
maringoding <adrimanalu5112@gmail.com>
2024-05-30 18:22:52 +0700

    Tugas 13 Laravel

---
+++ Tugas13Laravel/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+    Selamat datang
+@endsection
+
+@section('content')
+
+<h1>Selamat Datang Adri Yatniel Manalu! <?php echo $fname; ?> <?php echo $lname; ?></h1>
+<h3>Terima kasih telah bergabung di sanberbook. Social Media kita bersama!</h3>
+@endsection
